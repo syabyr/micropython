f453d6c4c7dae1fa32f2a09b3e0f9130ac9f1c38
syabyr <mybays@sina.com>
2026-05-30 01:58:27 +0800

    v1.24.1

---
+++ ports/efr32mg1p/boards/ikea/board_pins.h
@@ -0,0 +1,6 @@
+#define USART_TX_PORT gpioPortB
+#define USART_TX_PIN 15
+#define USART_TX_LOCATION 10 // ? found in af_pins.h somehow
+#define USART_RX_PORT gpioPortB
+#define USART_RX_PIN 14
+#define USART_RX_LOCATION 8 // ?
